36cac0ab459c71f2bc0308d3dd177da69251b515
Guilherme Souza Ferrari <guilhermesouzaferrari@gmail.com>
2024-10-22 11:43:42 -0300

    Resolvendo conflitos e corrigindo erros

--- scriptInicial.js
+++ scriptInicial.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     var downloadReportBtn = document.getElementById("downloadReportBtn");
     var reportModal = document.getElementById("reportModal");
@@ -46,53 +45,4 @@ document.addEventListener("DOMContentLoaded", function() {
             options[i].selected = selectAllCheckbox.checked;
         }
     });
-=======
-document.addEventListener("DOMContentLoaded", function() {
-    var downloadReportBtn = document.getElementById("downloadReportBtn");
-    var reportModal = document.getElementById("reportModal");
-    var closeBtn = document.getElementsByClassName("close")[0];
-    var selectAllCheckbox = document.getElementById("selectAll");
-    var freezerSelect = document.getElementById("freezerSelect");
-    var usernameSpan = document.getElementById("usernameSpan");
-
-    // Função para exibir o nome do usuário
-    function showUsername() {
-        const username = localStorage.getItem("username"); // Recupera o nome do usuário do local storage
-        if (usernameSpan) {
-            usernameSpan.textContent += username;
-        }
-    }
-
-    showUsername(); // Chama a função para exibir o nome do usuário
-
-    downloadReportBtn.onclick = function() {
-        reportModal.style.display = "block";
-        setTimeout(function() {
-            reportModal.classList.add("show");
-        }, 10); // Pequeno atraso para permitir que o display: block aplique antes da transição
-    };
-
-    closeBtn.onclick = function() {
-        reportModal.classList.remove("show");
-        setTimeout(function() {
-            reportModal.style.display = "none";
-        }, 500); // Tempo para permitir que a transição de opacidade termine antes de esconder
-    };
-
-    window.onclick = function(event) {
-        if (event.target == reportModal) {
-            reportModal.classList.remove("show");
-            setTimeout(function() {
-                reportModal.style.display = "none";
-            }, 500);
-        }
-    };
-
-    selectAllCheckbox.addEventListener("change", function() {
-        var options = freezerSelect && freezerSelect.options;
-        for (var i = 0; i < options.length; i++) {
-            options[i].selected = selectAllCheckbox.checked;
-        }
-    });
->>>>>>> origin/main
 });
