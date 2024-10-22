77895be896a58dfe3b37e476ff3043d49d62132e
Guilherme Souza Ferrari <guilhermesouzaferrari@gmail.com>
2024-10-22 16:54:48 -0300

    att

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,31 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll("a");
     const loader = document.getElementById("loader");
-    const loginForm = document.getElementById("LoginForm");
 
     links.forEach(link => {
         link.addEventListener("click", function(event) {
             loader.style.visibility = "visible";
         });
     });
-
-    if (loginForm) {
-        loginForm.addEventListener("submit", function(event) {
-            event.preventDefault();
-            const username = document.getElementById("usuario").value;
-            const password = document.getElementById("senha").value;
-            const validUsername = "desenvolvedor";
-            const validPassword = "123459876";
-
-            if (username === validUsername && password === validPassword) {
-                localStorage.setItem("username", username); // Armazena o nome do usuário no local storage
-                loader.style.visibility = "visible"; // Mostra o loader
-                setTimeout(function() {
-                    window.location.href = "inicial.html"; // Redireciona após 1 segundo
-                }, 1000);
-            } else {
-                alert("Usuário ou senha incorretos.");
-            }
-        });
-    }
 });
+
+function login(){
+    firebase.auth().signInWithEmailAndPassword("desenvolvedor@gmail.com", "123456789").then(Response => {
+        console.log('sucess', Response)
+    }).catch(error => {
+        console.log('error', error)
+    });
+}
