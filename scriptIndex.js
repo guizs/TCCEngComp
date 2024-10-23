436b1bc7d5d031cd32298040e63555359df394b8
Guilherme Souza Ferrari <guilhermesouzaferrari@gmail.com>
2024-10-23 14:00:29 -0300

    Atualização do login Firebase

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,18 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("a");
+function login(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value;
+
+    const loader = document.getElementById("loader"); // Pega o elemento do loader
+
+    firebase.auth().signInWithEmailAndPassword(email, senha)
+    .then(response => {
+        loader.style.visibility = "visible";
+
+        setTimeout(() => {
+            window.location.href = "inicial.html";
+        }, 3000);
+    })
+    .catch(error => {
+        alert(getErrorMessage(error));
+    });
+}
+
+function RecuperarSenha(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
     const loader = document.getElementById("loader");
 
-    links.forEach(link => {
-        link.addEventListener("click", function(event) {
-            loader.style.visibility = "visible";
-        });
-    });
-});
+    loader.style.visibility = "visible";
 
-function login(){
-    firebase.auth().signInWithEmailAndPassword("desenvolvedor@gmail.com", "123456789").then(Response => {
-        console.log('sucess', Response)
-    }).catch(error => {
-        console.log('error', error)
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        loader.style.visibility = "hidden";
+        alert('Email de recuperação enviado com sucesso.');
+    })
+    .catch(error => {
+        loader.style.visibility = "hidden";
+        if (error.code === 'auth/user-not-found') {
+            alert('Não há nenhum usuário cadastrado com este email.');
+            return;
+        } else if (error.code === 'auth/invalid-email') {
+            alert('E-mail inserido em um formato inexistente.');
+            return;
+        } else {
+            alert(getErrorMessage(error));
+        }
     });
+}
+
+function getErrorMessage(error) {
+    if (error.code === "auth/invalid-credential") {
+        return "E-mail ou senha inválidos.";
+    }
+    return error.message;
 }
